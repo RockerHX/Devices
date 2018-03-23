16f5b285515065003ec7d0d69369228146059ae2
RockerHX <rockerhx@gmail.com>
2018-03-23 11:48:03 +0800

    Initial Commit

---
+++ EasyBoy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyBoy
+//
+//  Created by RockerHX on 2018/3/23.
+//  Copyright © 2018年 RockerHX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
