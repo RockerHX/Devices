7bdb6b3413e692810299cb5c1967237b48f0b2d5
RockerHX <rockerhx@gmail.com>
2018-03-28 15:59:24 +0800

    Add[Code]: Demo Code
    Family code.
    Model code.
    Size code.

--- Demo/DevicesDemo/ViewController.swift
+++ Demo/DevicesDemo/ViewController.swift
@@ -16,23 +16,142 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("device: \(Device.family())")
-        switch Device.model() {
-        case .simulator(let model):
-            switch model {
-            case .iPhone8:
-                print("device: \(Device.model())")
-            default:
-                break;
-            }
-        default:
-            break
+        if Device.model().isPhone {
+            print(Device.model().description)
         }
 
-        if Device.model() == .simulator(.iPhone8) {
-            print("iPhone8")
-        }
+        #if os(iOS)
+        iOSDeviceFamily()
+        iOSDeviceModel()
+        iOSDeviceSize()
+        #elseif os(OSX)
+
+        #elseif os(tvOS)
+
+        #elseif os(watchOS)
+
+        #endif
         
+    }
+
+    func iOSDeviceFamily() {
+        let family = Device.family()
+        switch family {
+        case .iPodtouch:
+            print("Device belong to \(family) family")
+        case .iPhone:
+            print("Device belong to \(family) family")
+        case .iPad:
+            print("Device belong to \(family) family")
+        case .simulator:
+            print("Device belong to \(family) family")
+        case .unknown:
+            print("unknown Device.")
+        }
+    }
+
+    func iOSDeviceModel() {
+        let model = Device.model()
+        print(model.identifier)
+        switch model {
+        /*** iPod ***/
+        case .iPodTouch5Gen:
+            print("Device is a \(model)")
+        case .iPodTouch6Gen:
+            print("Device is a \(model)")
+        /*** iPhone ***/
+        case .iPhone4:
+            print("Device is a \(model)")
+        case .iPhone4S:
+            print("Device is a \(model)")
+        case .iPhone5:
+            print("Device is a \(model)")
+        case .iPhone5C:
+            print("Device is a \(model)")
+        case .iPhone5S:
+            print("Device is a \(model)")
+        case .iPhone6:
+            print("Device is a \(model)")
+        case .iPhone6Plus:
+            print("Device is a \(model)")
+        case .iPhone6S:
+            print("Device is a \(model)")
+        case .iPhone6SPlus:
+            print("Device is a \(model)")
+        case .iPhoneSE:
+            print("Device is a \(model)")
+        case .iPhone7:
+            print("Device is a \(model)")
+        case .iPhone7Plus:
+            print("Device is a \(model)")
+        case .iPhone8:
+            print("Device is a \(model)")
+        case .iPhone8Plus:
+            print("Device is a \(model)")
+        case .iPhoneX:
+            print("Device is a \(model)")
+        /*** iPad ***/
+        case .iPad3:
+            print("Device is a \(model)")
+        case .iPad4:
+            print("Device is a \(model)")
+        case .iPad5:
+            print("Device is a \(model)")
+        case .iPadAir:
+            print("Device is a \(model)")
+        case .iPadAir2:
+            print("Device is a \(model)")
+        case .iPadMini:
+            print("Device is a \(model)")
+        case .iPadMini2:
+            print("Device is a \(model)")
+        case .iPadMini3:
+            print("Device is a \(model)")
+        case .iPadMini4:
+            print("Device is a \(model)")
+        case .iPadPro9_7Inch:
+            print("Device is a \(model)")
+        case .iPadPro12_9Inch:
+            print("Device is a \(model)")
+        case .iPadPro12_9Inch2:
+            print("Device is a \(model)")
+        case .iPadPro10_5Inch:
+            print("Device is a \(model)")
+        /*** HomePod ***/
+        case .HomePod:
+            print("Device is a \(model)")
+        case .simulator(let model):
+            print("Device is a \(model)")
+        /*** unknown ***/
+        case .unknown:
+            print("unknown Device.")
+        }
+    }
+
+    func iOSDeviceSize() {
+        let size = Device.size()
+        switch size {
+        case .screen3_5Inch:
+            print("Device size: \(size.description)")
+        case .screen4Inch:
+            print("Device size: \(size.description)")
+        case .screen4_7Inch:
+            print("Device size: \(size.description)")
+        case .screen5_5Inch:
+            print("Device size: \(size.description)")
+        case .screen5_8Inch:
+            print("Device size: \(size.description)")
+        case .screen7_9Inch:
+            print("Device size: \(size.description)")
+        case .screen9_7Inch:
+            print("Device size: \(size.description)")
+        case .screen10_5Inch:
+            print("Device size: \(size.description)")
+        case .screen12_9Inch:
+            print("Device size: \(size.description)")
+        case .unknown:
+            print("Device size unknown.")
+        }
     }
 
 }
