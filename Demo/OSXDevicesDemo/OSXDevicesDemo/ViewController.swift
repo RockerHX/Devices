bb0947c6ae854e7a8c7546c79b4135fc54332f1a
RockerHX <rockerhx@gmail.com>
2018-03-29 16:12:34 +0800

    Update[Demo]: OSX

--- Demo/OSXDevicesDemo/OSXDevicesDemo/ViewController.swift
+++ Demo/OSXDevicesDemo/OSXDevicesDemo/ViewController.swift
@@ -8,6 +8,7 @@
 
 
 import Cocoa
+import Devices
 
 
 class ViewController: NSViewController {
@@ -15,11 +16,192 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print(Device.family())
-        print(Device.model())
-        print(Device.size())
-        print(Device.version())
+        #if os(iOS)
+        #elseif os(OSX)
+        OSXDeviceFamily()
+        OSXDeviceModel()
+        OSXModelHelper()
+        OSXDeviceSize()
+        OSXSizeHelper()
+        #elseif os(tvOS)
+
+        #elseif os(watchOS)
+
+        #endif
+
+    }
+
+    func OSXDeviceFamily() {
+        let family = Device.family()
+        switch family {
+        case .MacMini:
+            print("Device belong to \(family) family")
+        case .MacBookAir:
+            print("Device belong to \(family) family")
+        case .MacBook:
+            print("Device belong to \(family) family")
+        case .MacBookPro:
+            print("Device belong to \(family) family")
+        case .iMac:
+            print("Device belong to \(family) family")
+        case .iMacPro:
+            print("Device belong to \(family) family")
+        case .MacPro:
+            print("Device belong to \(family) family")
+        case .XServe:
+            print("Device belong to \(family) family")
+        default:
+            print("unknown Device.")
+        }
+    }
+
+    func OSXDeviceModel() {
+        let model = Device.model()
+        print(model.identifier)
+        switch model {
+        /*** Mac Mini ***/
+        case .MacMini2012Late:
+            print("Device is a \(model)")
+        case .MacMini2014Late:
+            print("Device is a \(model)")
+        /*** MacBook Air ***/
+        case .MacBookAir2012Mid:
+            print("Device is a \(model)")
+        case .MacBookAir2013MidAnd2014Early:
+            print("Device is a \(model)")
+        case .MacBookAir2015Early:
+            print("Device is a \(model)")
+        case .MacBookAir2017Mid:
+            print("Device is a \(model)")
+        /*** MacBook ***/
+        case .MacBook2015Early:
+            print("Device is a \(model)")
+        case .MacBook2016Early:
+            print("Device is a \(model)")
+        case .MacBook2017Mid:
+            print("Device is a \(model)")
+        /*** MacBook Pro ***/
+        case .MacBookPro2010Mid:
+            print("Device is a \(model)")
+        case .MacBookPro2011:
+            print("Device is a \(model)")
+        case .MacBookPro2012Mid:
+            print("Device is a \(model)")
+        /*** MacBook Pro Retina ***/
+        case .MacBookProRetina2012Mid:
+            print("Device is a \(model)")
+        case .MacBookProRetina2012Late:
+            print("Device is a \(model)")
+        case .MacBookProRetina2014Mid:
+            print("Device is a \(model)")
+        case .MacBookProRetina2015Early:
+            print("Device is a \(model)")
+        case .MacBookProRetina2015Mid:
+            print("Device is a \(model)")
+        case .MacBookProRetina2016Late:
+            print("Device is a \(model)")
+        case .MacBookProRetina2017Mid:
+            print("Device is a \(model)")
+        /*** iMac ***/
+        case .iMac2012Late:
+            print("Device is a \(model)")
+        case .iMac2013Late:
+            print("Device is a \(model)")
+        case .iMac2014Mid:
+            print("Device is a \(model)")
+        case .iMac2015Late:
+            print("Device is a \(model)")
+        case .iMac2017:
+            print("Device is a \(model)")
+        /*** iMac Retina ***/
+        case .iMacRetina2014Late:
+            print("Device is a \(model)")
+        case .iMacRetina2015Late:
+            print("Device is a \(model)")
+        case .iMacRetina2017Mid:
+            print("Device is a \(model)")
+        /*** iMac Pro ***/
+        case .iMacPro:
+            print("Device is a \(model)")
+        /*** Mac Pro ***/
+        case .MacPro2012Mid:
+            print("Device is a \(model)")
+        case .MacPro2013Late:
+            print("Device is a \(model)")
+        case .MacPro2017Early:
+            print("Device is a \(model)")
+        case .simulator(let model):
+            print("Device is a \(model)")
+            /*** unknown ***/
+        case .unknown:
+            print("unknown Device.")
+        }
+    }
+
+    func OSXModelHelper() {
+        let model = Device.model()
+        let allMacBookProes = Device.Model.allMacBookProes
+        if allMacBookProes.contains(model) {
+            print("Current device belong to MacBookPro ")
+        }
+    }
+
+    func OSXDeviceSize() {
+        let size = Device.size()
+        switch size {
+        case .screen11Inch:
+            print("Device size: \(size.description)")
+        case .screen12Inch:
+            print("Device size: \(size.description)")
+        case .screen13Inch:
+            print("Device size: \(size.description)")
+        case .screen15Inch:
+            print("Device size: \(size.description)")
+        case .screen17Inch:
+            print("Device size: \(size.description)")
+        case .screen20Inch:
+            print("Device size: \(size.description)")
+        case .screen21_5Inch:
+            print("Device size: \(size.description)")
+        case .screen24Inch:
+            print("Device size: \(size.description)")
+        case .screen27Inch:
+            print("Device size: \(size.description)")
+        case .unknown:
+            print("Device size unknown.")
+        }
+    }
+
+    func OSXSizeHelper() {
+        let size = Device.size()
+
+        if size > .screen15Inch {
+            print("Your device screen is larger than 15 inch")
+        }
+
+        if size < .screen15Inch {
+            print("Your device screen is smaller than 15 inch")
+        }
+
+        if size >= .screen15Inch {
+            print("Your device screen is equal or larger than 15 inch")
+        }
+
+        if size <= .screen15Inch {
+            print("Your device screen is equal or smaller than 15 inch")
+        }
+
+        if size == .screen15Inch {
+            print("It's a 15 inch screen")
+        }
+
+        if size != .screen17Inch {
+            print("It's not 17 inch screen")
+        }
+
+        if Device.isRetina() {
+            print("It's a retina display")
+        }
     }
 
 }
-
