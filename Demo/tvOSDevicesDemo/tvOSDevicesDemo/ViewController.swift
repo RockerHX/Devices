f0d7255bd91d0a38dded2703851cc315273e8775
RockerHX <rockerhx@gmail.com>
2018-03-30 09:12:11 +0800

    Update[Demo]: tvOSDevicesDemo

--- Demo/tvOSDevicesDemo/tvOSDevicesDemo/ViewController.swift
+++ Demo/tvOSDevicesDemo/tvOSDevicesDemo/ViewController.swift
@@ -8,6 +8,7 @@
 
 
 import UIKit
+import Devices
 
 
 class ViewController: UIViewController {
@@ -15,10 +16,51 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print(Device.family())
-        print(Device.model())
+        #if os(iOS)
+        #elseif os(OSX)
+        #elseif os(tvOS)
+        tvOSDeviceFamily()
+        tvOSDeviceModel()
+        tvOSModelHelper()
+        #elseif os(watchOS)
+        #endif
+
     }
 
+    func tvOSDeviceFamily() {
+        let family = Device.family()
+        switch family {
+        case .AppleTV:
+            print("Device belong to \(family) family")
+        case .simulator:
+            print("Device belong to \(family) family")
+        case .unknown:
+            print("unknown Device.")
+        }
+    }
+
+    func tvOSDeviceModel() {
+        let model = Device.model()
+        print(model.identifier)
+        switch model {
+        case .AppleTV4Gen:
+            print("Device is a \(model)")
+        case .AppleTV4K:
+            print("Device is a \(model)")
+        case .simulator(let model):
+            print("Device is a \(model)")
+        case .unknown:
+            print("unknown Device.")
+        }
+    }
+
+    func tvOSModelHelper() {
+        let model = Device.model()
+        let allTVs = Device.Model.allTVs
+        if allTVs.contains(model) {
+            print("Current device belong to Apple TV ")
+        }
+    }
 
 }
 
