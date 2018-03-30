036bbd4d6203c082a09b6a811c3dc2cd967f0a29
RockerHX <rockerhx@gmail.com>
2018-03-30 10:18:24 +0800

    Add[Demo]: watchOSDevicesDemo

---
+++ Demo/watchOSDevicesDemo/watchOSDevicesDemo WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  watchOSDevicesDemo WatchKit Extension
+//
+//  Created by RockerHX on 2018/3/30.
+//  Copyright © 2018年 RockerHX. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Demo/watchOSDevicesDemo/watchOSDevicesDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  watchOSDevicesDemo
+//
+//  Created by RockerHX on 2018/3/30.
+//  Copyright © 2018年 RockerHX. All rights reserved.
+//
+
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
