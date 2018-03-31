2aeb8ce29f5289f01964f88b88146a42d0d6b0d2
RockerHX <rockerhx@gmail.com>
2018-03-31 16:33:21 +0800

    Update: Marks

--- Classes/Size.swift
+++ Classes/Size.swift
@@ -1,6 +1,6 @@
 //
 //  Size.swift
-//  Device
+//  Devices
 //
 //  Created by RockerHX on 2018/3/23.
 //  Copyright © 2018年 RockerHX. All rights reserved.
@@ -165,7 +165,6 @@ extension Device {
     static public func isRetina() -> Bool {
         return UIScreen.main.scale > 1.0
     }
-
 }
 
 
