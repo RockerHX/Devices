fcbb753cdc03846c3027d6dfa0232790c1eef37c
RockerHX <rockerhx@gmail.com>
2018-05-07 00:00:53 +0800

    Update[Demo]: iOSDevicesDemo

--- Demo/iOSDevicesDemo/iOSDevicesDemo/ViewController.swift
+++ Demo/iOSDevicesDemo/iOSDevicesDemo/ViewController.swift
@@ -234,6 +234,7 @@ class ViewController: UIViewController {
     func iOSApplication() {
         print("Build: \(Device.Application.shared.build)")
         print("Version: \(Device.Application.shared.version)")
+        print("name: \(Device.Application.shared.name)")
         print("wholeVersion: \(Device.Application.shared.wholeVersion)")
         print("firstLanuch: \(Device.Application.shared.firstLanuch)")
     }
