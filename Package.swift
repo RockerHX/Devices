1dee37239b8b318a8bb63f45723893532905c83c
RockerHX <rockerhx@gmail.com>
2021-04-27 17:29:27 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "Devices",
+    platforms: [.iOS(.v10), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v3)],
+    products: [
+        .library(
+            name: "Devices",
+            targets: ["Devices"])
+    ],
+    targets: [
+        .target(
+            name: "Devices",
+            path: "Classes")
+    ]
+)
